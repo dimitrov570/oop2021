6e9d1eadc3e89e509436e782d3d0d0060743e8d7
dimitrov570 <ivajlo.dimitrov99@gmail.com>
2021-02-25 10:33:34 +0100

    add more thing about static variables

--- week01/allignmentExample.cpp
+++ week01/allignmentExample.cpp
@@ -2,23 +2,30 @@
 using namespace std;
 
 struct Allignment1 {
+    static int q; //декларация на статичната промелнива q
     uint8_t a;
     uint16_t b;
     uint32_t c;
     uint8_t d;
 };
 
+int Allignment1::p = 0; //дефинира се статичната променлива q извън класа, преди да се използва
+
 struct Allignment2 {
-    static int p;
+    static int p; //декларация на статичната промелнива p
     uint8_t a;
     uint8_t d;
     uint16_t b;
     uint32_t c;
 } x,y; //създават се два обекта x и y
 
-int Allignment2::p = 0; //декларира се статичната променлива p, преди да се използва
+int Allignment2::p = 0; //дефинира се статичната променлива p извън класа, преди да се използва
 
 int main(){
+    
+    //статичните променливи можем да достъпим и без да има създадени обекти от този тип
+    //забележете, че от Allignment1 не са създавани обекти
+    cout << "Value of p: " << Allignment1::q << "\n";
 
     cout << "Size of Allignment1: " << sizeof(Allignment1) << "\n";
     cout << "Size of Allignment2: " << sizeof(Allignment2) << "\n";
