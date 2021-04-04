ca48da0185f130d52dc0b11af89443ab1a8518c9
dimitrov570 <ivajlo.dimitrov99@gmail.com>
2021-04-05 00:41:57 +0200

    remove echo.cpp

--- week07/cat.cpp
+++ week07/cat.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 
+const int BUFF_SIZE = 10;
 
-
-int main()
+void catBuffered(std::istream& is, std::ostream& os)
 {
+
+}
+
+void catByteByByte(std::istream& is, std::ostream& os)
+{
+    
+}
+
+int main(int argc, char* argv[])
+{
+    // std::cout << argc << '\n';
+    // std::cout << argv[0] << '\n';
+    // std::cout << argv[1] << '\n';
+    if (argc == 2)
+    {
+        
+    }
+    else {
+      catByteByByte(std::cin, std::cout);
+    }
 
 
     return 0;
