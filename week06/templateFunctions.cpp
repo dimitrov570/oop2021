f577de703d22118785142e860e20898e7f072820
dimitrov570 <ivajlo.dimitrov99@gmail.com>
2021-04-29 16:42:54 +0200

    add readme for templates

--- week06/templateFunctions.cpp
+++ week06/templateFunctions.cpp
@@ -4,11 +4,11 @@
 #include <set>
 
 
-//template min
+//template max
 template <typename T> 
-T min(T a, T b)
+T max(T a, T b)
 {
-    return a < b ? a : b;
+    return a > b ? a : b;
 }
 
 //template swap
